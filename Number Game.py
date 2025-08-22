b957e00633e38ce4359fae8942e32b0689377974
Jli369-code <lijin_369@yahoo.com>
2025-08-22 20:22:24 +1000

    Update Number Game.py

--- Number Game.py
+++ Number Game.py
@@ -1,18 +1,22 @@
 #Jin
-import random
-
 def number_game():
     print("\n--- Number Guessing Game ---")
+    import random
     target = random.randint(1, 100)
-    try:
-        guess = int(input("Guess a number between 1 and 100: "))
-        if guess > target:
-            print("Too high!")
-        elif guess < target:
-            print("Too low!")
-        else:
-            print("Correct! You guessed the number.")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
+    attempt = 0
+
+    while True:
+        try:
+            guess = int(input("Guess a number between 1 and 100: "))
+            attempt += 1
+            if guess > target:
+                print("Too high!")
+            elif guess < target:
+                print("Too low!")
+            else:
+                print("Correct! You guessed the number in " , attempt, "attempts and the number is " , target)
+                break                
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 number_game()
