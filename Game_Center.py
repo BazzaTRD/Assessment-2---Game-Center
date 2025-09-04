f339f4444babe6cd5718031ce591cfe515894e9d
BazzaTRD <bazzatrd@gmail.com>
2025-09-04 21:25:33 +1000

    hopefully the final change...

--- Game_Center.py
+++ Game_Center.py
@@ -21,15 +21,18 @@ def write_result(game_num, result, user_file):
             list_file_content[5] += 1
     elif game_num == 3: #Sorting game
         if result == True:
+            print("sorting game true")
             list_file_content[7] = int(list_file_content[7]) + 1
         elif result == False:
+            print("sorting game false")
             list_file_content[8] = int(list_file_content[8]) + 1
+    print(list_file_content)
     with open(user_file, "w") as file:
             for content in list_file_content:
                 try:
-                    file.write(content + "")
+                    file.write(content + "\n")
                 except TypeError:
-                    file.write(str(content) + "")
+                    file.write(str(content) + "\n")
 
 
 def read_result(user_file):
@@ -39,22 +42,27 @@ def read_result(user_file):
     
     list_file_content = []
 
-    print(f"\nYour current results are:")
     try:
         #ensure the contents of the file are as expected
         with open(user_file) as file:
             for i, content in enumerate(file.readlines()):
-                list_file_content.append(content)
+                list_file_content.append(content.strip())
                 match i:
                     case 0 | 3 | 6:
                         if content.strip() != list_template[i]:
                             print(f"'{content.strip()}' Does not exist... Expected '{list_template[i]}'")
                             raise ValueError
-                        print(f"---{content.strip()}---")
                     case 1 | 4 | 7:
-                        print(f"Wins: {int(content.strip())}")
+                        int(content.strip())
                     case 2 | 5 | 8:
-                        print(f"Loss: {int(content.strip())}")
+                        int(content.strip())
+                    case _:
+                        break
+        while len(list_file_content) < 9:
+            #print("less than 9")
+            list_file_content.append("0")
+            with open(user_file, "a") as file:
+                file.write("\n0")
     except ValueError:
         #overwrite the existing contents with a template
         print("Unexpected value... Overwriting existing content with basic values")
@@ -62,7 +70,9 @@ def read_result(user_file):
             for content in list_template:
                 file.write(content + "\n")
         list_file_content = list_template
+    #print(list_file_content)
     return list_file_content
+
 
 
 def user():
@@ -88,7 +98,20 @@ def user():
             #Try existing user
             user_name = input("\nEnter your username: ").lower()
             user_file = os.path.join(users_dir, user_name + ".txt") # .txt File path
-            read_result(user_file)
+            list_file_content = read_result(user_file)
+            
+            print(f"\nYour current results are:")
+            for i, content in enumerate(list_file_content):
+                match i:
+                    case 0 | 3 | 6:
+                        print(f"---{content}---")
+                    case 1 | 4 | 7:
+                        print(f"Wins: {content}")
+                    case 2 | 5 | 8:
+                        print(f"Loss: {content}")
+                    case _:
+                        print("toes")
+                        break 
             return [user_name, user_file]
         except OSError:
             #Create a new user
@@ -121,28 +144,41 @@ def main():
         print("4. Logan's Game")
         print("5. Results")
         print("6. Exit")
-        choice = input("Enter your choice (1-6): ")
+        choice = input("Enter your choice (1-6): ").lower()
 
         #User choices
         #need to somehow parse the 'user_file' argument to these others
-        if choice == "0":
-            with open("help.txt") as file:
-                print(file.read())
-        elif choice == "1":
-            subprocess.run(["python", "Number Game.py"])
-        elif choice == "2":
-            subprocess.run(["python", "Rock Paper Scissors.py"])
-        elif choice == "3":
-            subprocess.run(["python", "Sorting Game.py", user_file[1]])
-        #elif choice == "4":
-            #subprocess.run(["python", "Sorting Game.py"])
-        elif choice == "5":
-            read_result(user_file[1])
-        elif choice == "6":
-            print("Thanks for playing. Goodbye!")
-            break
-        else:
-            print("Invalid choice. Try again.")
+        match choice:
+            case "0" | "help":
+                with open("help.txt") as file:
+                    print(file.read())
+            case "1":
+                subprocess.run(["python", "Number Game.py"])
+            case "2":
+                subprocess.run(["python", "Rock Paper Scissors.py"])
+            case "3":
+                subprocess.run(["python", "Sorting Game.py", user_file[1]])
+            #case "4":
+                #subprocess.run(["python", "Sorting Game.py"])
+            case "5" | "result" | "results":
+                print(f"\nYour current results are:")
+                with open(user_file[1]) as file:
+                    for i, content in enumerate(file.readlines()):
+                        match i:
+                            case 0 | 3 | 6:
+                                print(f"---{content.strip()}---")
+                            case 1 | 4 | 7:
+                                print(f"Wins: {content.strip()}")
+                            case 2 | 5 | 8:
+                                print(f"Loss: {content.strip()}")
+                            case _:
+                                print("toes")
+                                break 
+            case "6" | "exit":
+                print("Thanks for playing. Goodbye!")
+                break
+            case _:
+                print("Invalid choice. Try again.")
         
         #Play agin?
         again = input("\nWould you like to play another game? (yes/no): ").lower()
